52f2cc92a1a8c68a34e2968d49432650602c6c63
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-16 10:53:11 +0530

    Birb movement

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pygame
+from objects import Birb
 app = pygame.display.set_mode((640, 480))
 
+birb = Birb(100, 100)
+velocity = [0, 0]
+acceralation = [0.1, 0.1]
+jumpDown = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        if not jumpDown:
+            velocity[1] -= 10
+            jumpDown = True
+    else:
+        jumpDown = False
     app.fill((0, 0, 0))
+    birb.y += velocity[1]
+    velocity[1] += acceralation[1]
+    birb.draw(app)
     pygame.display.update()
     pygame.time.delay(10)
