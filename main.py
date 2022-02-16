7b5a5571a0712ac1eae9d26cd89239a1155f21ce
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-16 14:58:11 +0530

    pillar spawning and rendering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from objects import Birb
+from objects import Birb, Pillar
+import random
 app = pygame.display.set_mode((640, 480))
 
 birb = Birb(100, 100)
@@ -8,6 +9,13 @@ acceralation = [0.1, 0.1]
 jumpDown = False
 BACKGROUND = pygame.image.load("assets/art/background.png")
 run = True
+pillars = []
+
+def createPillar():
+    x = 650
+    y = random.randint(-350, -150)
+    pillar = Pillar(x, y)
+    pillars.append(pillar)
 
 while run:
     for event in pygame.event.get():
@@ -24,5 +32,20 @@ while run:
     birb.y += velocity[1]
     velocity[1] += acceralation[1]
     birb.draw(app)
+    #rendering pillars
+    if len(pillars) < 4:
+        if len(pillars) > 0:
+            if 450 > pillars[-1].x + pillars[-1].pillar.get_width():
+                createPillar()
+        else:
+            createPillar()
+        
+    for pillar in pillars:
+        if pillar.x < -50:
+            pillars.remove(pillar)
+        pillar.x -= 2
+        pillar.draw(app)
+    
+    
     pygame.display.update()
     pygame.time.delay(10)
--- objects.py
+++ objects.py
@@ -8,3 +8,12 @@ class Birb:
     
     def draw(self, screen):
         screen.blit(self.birb, (self.x, self.y))
+
+class Pillar:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.pillar = pygame.image.load("assets/art/pillar.png").convert_alpha()
+    
+    def draw(self, screen):
+        screen.blit(self.pillar, (self.x, self.y))
