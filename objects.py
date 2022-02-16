659747debc6e844f046bb97e3e99dd736e43a977
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-16 10:52:22 +0530

    Birb class

---
+++ objects.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Birb:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), 10)
